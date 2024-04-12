0f3f6af281f6944ec33bb4af18d8fb086800f359
SantiagoDLCB <sa.delacarrera@uc.cl>
2024-04-12 13:27:02 -0400

    Devise instalado

--- Gemfile
+++ Gemfile
@@ -69,3 +69,5 @@ group :test do
 end
 
 gem 'rubocop', require: false
+
+gem "devise"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+ 
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, 
+                   path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+
   get 'render/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

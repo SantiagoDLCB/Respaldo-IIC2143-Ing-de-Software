0b6a677f82eb8950322698242661f78b749667ea
SantiagoDLCB <sa.delacarrera@uc.cl>
2024-04-15 00:23:39 -0400

    Verificacion mail uc

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, presence: true, format: { with: /\A[^@\s]+@uc\.cl\z/, message: "El mail debe ser un Mail UC" }
 end

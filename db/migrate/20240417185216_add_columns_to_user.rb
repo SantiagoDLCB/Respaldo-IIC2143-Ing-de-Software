956afb56aef7250f4c1693dff0a97f9f86379cf2
SantiagoDLCB <sa.delacarrera@uc.cl>
2024-04-17 14:53:59 -0400

    Nuevas columnas usuario

---
+++ db/migrate/20240417185216_add_columns_to_user.rb
@@ -0,0 +1,8 @@
+class AddColumnsToUser < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    add_column :users, :name, :string
+    add_column :users, :last_name, :string
+  end
+end

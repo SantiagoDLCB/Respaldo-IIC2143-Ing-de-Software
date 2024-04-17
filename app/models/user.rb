5928549680a6f7bbe407021127fa7c941ba3b67c
SantiagoDLCB <sa.delacarrera@uc.cl>
2024-04-17 15:54:26 -0400

    Validaciones de datos

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :email, presence: true, format: { with: /\A[^@\s]+@uc\.cl\z/, message: "El mail debe ser un Mail UC" }
   validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :last_name, presence: true
 end

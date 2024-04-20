f8470b972b22c799bd0f5060de551b928121fd84
SantiagoDLCB <sa.delacarrera@uc.cl>
2024-04-20 12:59:31 -0400

    Solicitudes enviables y cancelables

--- app/models/initiative.rb
+++ app/models/initiative.rb
@@ -2,10 +2,11 @@ class Initiative < ApplicationRecord
   resourcify
   has_many :roles, class_name: "Role", as: :resource
   has_many :users, through: :roles, source: :users
+  has_many :requests
   def self.all_roles
     Role.where(resource_type: 'Initiative')
   end
-  
+
   validates :name, uniqueness: true,  presence: true
   validates :topic, presence: true
   validates :description, presence: true
@@ -13,5 +14,9 @@ class Initiative < ApplicationRecord
   def get_admin
     roles.find_by(name: 'admin_initiative')&.users.first
   end
- 
+
+  def get_request(user)
+    requests.where(user: user).last
+  end
+
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :initiative
-  enum status: { pending: 'Pendiente', accepted: 'Aceptada', denied: 'Rechazada' }
+  enum status: { pending: 'Pendiente', accepted: 'Aceptada', denied: 'Rechazada', :cancelled => 'Cancelada'}
 end

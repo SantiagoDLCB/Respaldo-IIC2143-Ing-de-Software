77c4593ebef7997ccfd668c01c1b3197f73b77ce
andresinfantep <a.infante@uc.cl>
2024-04-22 17:41:41 -0400

    eliminar evento arreglado

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,12 +7,12 @@ class EventsController < ApplicationController
 
   def create
     @event = @initiative.events.build(event_params)
+    current_user.add_role :admin_event, @event
     if @event.save
       redirect_to initiative_path(@initiative.id), notice: 'Evento creado exitosamente.'
     else
       redirect_to initiative_path(@initiative.id), notice: 'Evento no ha podido ser creado.'
     end
-    current_user.add_role :admin_event, @event
   end
   def set_initiative
     @initiative = Initiative.find(params[:initiative_id])
@@ -20,6 +20,12 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @initiative = @event.initiative
+  end
+  def destroy
+    @event = Event.find(params[:id])
+    @initiative = @event.initiative
+    @event.destroy
+    redirect_to initiative_path(@initiative), notice: 'El evento fue eliminado.'
   end
 
   def event_params

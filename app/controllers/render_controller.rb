9bbbff136fcdbebd67196ca3d088a250f19bd30b
VicenteSanMartin <vjsm@uc.cl>
2024-04-25 18:06:12 -0400

    test rubocop

--- app/controllers/render_controller.rb
+++ app/controllers/render_controller.rb
@@ -3,14 +3,12 @@
 class RenderController < ApplicationController
   before_action :redirect_to_initiatives, if: :user_signed_in?
 
-
   def index
     @recent_initiatives = Initiative.order(created_at: :desc).limit(3)
   end
 
   private
-  
-    def redirect_to_initiatives
-      redirect_to initiatives_path
-    end
+  def redirect_to_initiatives
+    redirect_to initiatives_path
+  end
 end

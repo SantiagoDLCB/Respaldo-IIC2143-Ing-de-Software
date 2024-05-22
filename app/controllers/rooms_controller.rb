fe6b49ae24aaa99d95c3a62c414f276501400e30
SantiagoDLCB <sa.delacarrera@uc.cl>
2024-05-22 12:14:37 -0400

    manejo de imagenes mejorado

--- app/controllers/rooms_controller.rb
+++
@@ -1,9 +0,0 @@
-class RoomsController < ApplicationController
-
-  def index
-    @current_user = current_user
-    redirect_to '/signin' unless @current_user
-    @rooms = Room.public_rooms
-    @users = User.all_except(@current_user)
-  end
-end

0b4b6b1262f0299e202a02b1bb212742435a1c81
SantiagoDLCB <sa.delacarrera@uc.cl>
2024-05-26 17:32:46 -0400

    Nuevas credenciales

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,37 @@
 
 unless User.exists?(username: 'Dios')
   # Create example user
-  user = User.new(email: 'jmun@uc.cl', password: 'chavales', password_confirmation: 'chavales', name: 'Jorge', last_name: 'Muñoz', username: 'Dios')
-
+  user = User.new(email: 'jmun@uc.cl', password: 'DCChavales', password_confirmation: 'DCChavales', name: 'Jorge', last_name: 'Muñoz', username: 'Dios')
   user.save!
   user.add_role(:admin)
   puts "Admin seeded successfully."
 else
   puts "Admin already exists. Skipping seeding."
+end
+
+unless User.exists?(username: 'vjsm')
+  # Create example user
+  user = User.new(email: 'vjsm@uc.cl', password: 'DCClave', password_confirmation: 'DCClave', name: 'Vicente', last_name: 'San Martín', username: 'vjsm')
+  user.save!
+  puts "vjsm seeded successfully."
+else
+  puts "vjsm already exists. Skipping seeding."
+end
+
+unless User.exists?(username: 'ainfantep')
+  # Create example user
+  user = User.new(email: 'ainfante@uc.cl', password: 'DCContraseña', password_confirmation: 'DCContraseña', name: 'Andres', last_name: 'Infante', username: 'ainfantep')
+  user.save!
+  puts "ainfantep seeded successfully."
+else
+  puts "ainfantep already exists. Skipping seeding."
+end
+
+unless User.exists?(username: 'sa.dlcb')
+  # Create example user
+  user = User.new(email: 'sa.delacarrera@uc.cl', password: 'DCCifrado', password_confirmation: 'DCCifrado', name: 'Santiago', last_name: 'de la Carrera', username: 'sa.dlcb')
+  user.save!
+  puts "sa.dlcb seeded successfully."
+else
+  puts "sa.dlcb already exists. Skipping seeding."
 end

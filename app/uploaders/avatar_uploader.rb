ac8cd4f7ca0e7f8de6296616fb0bb965549e5202
SantiagoDLCB <sa.delacarrera@uc.cl>
2024-06-07 01:36:47 -0400

    fix cloudinary

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,15 +2,18 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
+  include Cloudinary::CarrierWave
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # Procesamiento opcional
+  process convert: 'jpg'
+  process tags: ['post_picture']
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,15 +3,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # Procesamiento opcional
+  process convert: 'jpg'
+  process tags: ['post_picture']
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
